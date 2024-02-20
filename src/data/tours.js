de92f598d872ac2488909f294b1a211552d37735
Manan Bansal <mananbansal143@gmail.com>
2024-02-20 12:02:56 -0800

    image update in 1st package

--- src/data/tours.js
+++ src/data/tours.js
@@ -2,7 +2,7 @@ const tours = [
   {
     id: 1,
     tag: "LIKELY TO SELL OUT*",
-    slideImg: ["/img/tours/Untitled design (1).png","/img/tours/Yamunotri (2).png","/img/tours/Gangotri (1).png", "/img/tours/img/tours/kedarnath 11.png",],
+    slideImg: ["/img/tours/Untitled design (1).png","/img/tours/Yamunotri (2).png","/img/tours/Gangotri (1).png", "/img/tours/kedarnath 15.png",],
     title: "Char Dham Yatra with tungnath Ex Delhi 11N/12D",
     location: "Uttrakhand, India",
     duration: "12",
